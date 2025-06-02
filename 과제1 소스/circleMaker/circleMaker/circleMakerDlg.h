750a1e75cb2ba276777c3738a1248b4d0331ef44
JHK-maehsay <jeokjeolz@gmail.com>
2025-06-02 12:41:53 +0900

    과제1 프로젝트 생성

---
+++ 과제1 소스/circleMaker/circleMaker/circleMakerDlg.h
@@ -0,0 +1,34 @@
+
+// circleMakerDlg.h : header file
+//
+
+#pragma once
+
+
+// CcircleMakerDlg dialog
+class CcircleMakerDlg : public CDialogEx
+{
+// Construction
+public:
+	CcircleMakerDlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CIRCLEMAKER_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
